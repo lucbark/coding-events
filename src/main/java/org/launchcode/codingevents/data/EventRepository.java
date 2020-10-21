955d86f812327f860d39650e4e9904ea3862dc11
Cas <testing@owners-MacBook-Pro-2.local>
2020-10-21 11:56:16 -0500

    Finished Chapter 17 example

--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface EventRepository extends CrudRepository<Event, Integer> {
-}
+
+} //this special interface allows code to store and retrieve object instances from a database of a specific type
+
